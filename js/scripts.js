a633be8d0282cf05742a6d4e158c6574c88908db
Juancaceres963 <dnijuancaceres@gmail.com>
2023-03-09 14:16:39 -0300

    Error 404 solucionado

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('#menu_on').click(function(){ // Al hacer click...
         $('body').toggleClass('visible_menu'); // Añadimos o eliminamos la clase 'visible_menu' al body
+      return false;
       })
 });
