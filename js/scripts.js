c29c6cff7583e35a319d79755202dc57c8d8b82f
Juancaceres963 <dnijuancaceres@gmail.com>
2023-03-09 18:13:48 -0300

    Div de inicio y fin de paginas

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
+  // Fuccion de Hamburguesa
     $('#menu_on').click(function(){ // Al hacer click...
         $('body').toggleClass('visible_menu'); // Añadimos o eliminamos la clase 'visible_menu' al body
       return false;
       })
+  // 
 });
