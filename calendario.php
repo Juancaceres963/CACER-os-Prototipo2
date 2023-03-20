380c1c9efd56628925eafd885e20377491d2242f
Juancaceres963 <dnijuancaceres@gmail.com>
2023-03-20 16:38:16 -0300

    Foreach para imprimir en la web

--- calendario.php
+++ calendario.php
@@ -5,7 +5,7 @@
     <?php
     try {
         require_once('./includes/funciones/bd_conexion.php');  //  1) Creamos la conexión 
-        $sql = "SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado ";  //  2) Escribimos la consulta
+        $sql = "SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, icono, nombre_invitado, apellido_invitado ";  //  2) Escribimos la consulta
         $sql .= " FROM eventos ";
         $sql .= " INNER JOIN categoria_evento ";
         $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
@@ -21,41 +21,48 @@
     <div class="calendario">
         <?php
         $calendario = array();
-        while ( $eventos = $resultado->fetch(PDO::FETCH_ASSOC) ) {  //4) Imprimimos los resultados
+        while ($eventos = $resultado->fetch(PDO::FETCH_ASSOC)) {  //4) Imprimimos los resultados
             $fecha = $eventos['fecha_evento'];
-            
+
             $evento = array(
                 'titulo' => $eventos['nombre_evento'],
                 'fecha' => $eventos['fecha_evento'],
                 'hora' => $eventos['hora_evento'],
                 'categoria' => $eventos['cat_evento'],
+                'icono' => $eventos['icono'],
                 'invitado' => $eventos['nombre_invitado'] . ' ' . $eventos['apellido_invitado']
             );
 
             $calendario[$fecha][] = $evento;
 
-            ?>
-            <?php } ?> <!-- FETCH_ASSOC -->
+        ?>
+        <?php } ?> <!-- FETCH_ASSOC -->
 
-            <?php 
-                // Imprimir las fechas de los enventos 
-                foreach($calendario as $dia => $lista_eventos) { ?>
-                <h3>
-                    <i class="fa-regular fa-calendar-days"></i>
-                    <?php 
-                    //Unix
-                    setlocale(LC_TIME, "es_ES.UTF-8");
-                    // Windows 
-                    setlocale(LC_TIME, "spanish.UTF-8");
-                    
-                    echo strftime( "%A %d de %B del %Y", strtotime($dia) );?>
-                </h3>
-               <?php }  ?>
+        <?php
+        // Imprimir las fechas de los enventos 
+        foreach ($calendario as $dia => $lista_eventos) { ?>
+            <h3>
+                <i class="fa-regular fa-calendar-days"></i>
+                <?php
+                //Unix
+                setlocale(LC_TIME, "es_ES.UTF-8");
+                // Windows 
+                setlocale(LC_TIME, "spanish.UTF-8");
 
-
-            <pre>
-                <?php var_dump($calendario); ?> <!-- var_dump es lo que nos permitira imprimir los resultados -->
-            </pre>
+                echo strftime("%A %d de %B del %Y", strtotime($dia)); ?>
+            </h3>
+            <?php foreach ($lista_eventos as $evento) { ?>
+                <div class="dia">
+                    <p><?php echo $evento["titulo"]?></p>
+                    <p><i class="fa-regular fa-calendar-days"></i> <?php echo $evento["fecha"]?></p>
+                    <p><i class="fa-solid fa-clock"></i> <?php echo $evento["hora"]?></p>
+                    <p><i class="<?php echo $evento["icono"]?>"></i> <?php echo $evento["categoria"]?></p>
+                    <p><i class="fa-solid fa-user"></i> <?php echo $evento["invitado"]?></p>
+                </div>
+            <?php } ?>
+           
+        <?php } // Fin foreach dias 
+        ?>
     </div>
 
     <?php
